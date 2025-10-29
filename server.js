ad0741679c52c7674e31b53222eefb1c875dea66
HimaVarshitha7 <231FA04A55@gmail.com>
2025-10-29 20:29:08 +0530

    secondcommit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://your-frontend-name.vercel.app" 
+    "https://scamt-frontend.vercel.app/" 
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
