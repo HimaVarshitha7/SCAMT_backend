9661db26bcf647a8aac76506fc57d297e4915ad6
HimaVarshitha7 <231FA04A55@gmail.com>
2025-10-29 20:18:36 +0530

    first commit

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const authRoutes = require('./routes/auth');
 const complaintRoutes = require('./routes/complaints');
 const feedbackRoutes = require('./routes/feedback');
 const serviceRoutes = require('./routes/services');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: [
+    "http://localhost:5173", 
+    "https://your-frontend-name.vercel.app" 
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
@@ -27,7 +31,14 @@ app.use('/api/complaints', complaintRoutes);
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/services', serviceRoutes);
 
+
 app.get('/', (req, res) => res.json({ status: 'ok' }));
 
+
+app.get('/testdb', async (req, res) => {
+  const state = mongoose.connection.readyState;
+  const status = ["disconnected", "connected", "connecting", "disconnecting"][state];
+  res.json({ dbStatus: status });
+});
 
 module.exports = app;
